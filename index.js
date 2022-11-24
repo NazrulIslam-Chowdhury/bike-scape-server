f415bca49cd96c527d1b223e7a97828fbbbf1a3b
rahiislam96 <nazrulislam66188@gmail.com>
2022-11-24 13:22:55 +0600

    api for advertise items created

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config();
@@ -8,6 +9,29 @@ require('dotenv').config();
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hnlrj23.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const sellingItemsCollection = client.db('bikeScape').collection('sellingItems');
+
+        app.get('/advertise', async (req, res) => {
+            const query = {};
+            const result = await sellingItemsCollection.find(query).toArray();
+            res.send(result);
+        })
+    }
+    finally {
+
+    }
+}
+
+run().catch(err => console.error(err));
+
 
 app.get('/', (req, res) => {
     res.send('Assignment 12 is running')
